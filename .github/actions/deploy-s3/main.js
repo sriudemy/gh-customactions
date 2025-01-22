b06fe59b81ff02f0f17d512c52df02417e9ff6ac
srinath1 <srinath.erp@gmail.com>
2025-01-22 19:56:01 +0100

    output url

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -9,7 +9,8 @@ function run() {
   const s3Uri = `s3://${bucket}`;
 
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-  core.notice("heelo to print from JS");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
