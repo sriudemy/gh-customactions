cef97ca679cf6619322def3987485363b1742309
srinath1 <srinath.erp@gmail.com>
2025-01-22 19:38:14 +0100

    configured s3

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -3,6 +3,12 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+  const s3Uri = `s3://${bucket}`;
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice("heelo to print from JS");
 }
 
